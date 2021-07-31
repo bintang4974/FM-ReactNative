88a0416b2755511ca813c9520ecba000a928a0ed
bintang4974 <bintangramadhan4974@gmail.com>
2021-07-31 10:46:28 +0700

    slicing splash screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import { SplashScreen } from './pages';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text>Hello Food Market App</Text>
-    </SafeAreaView>
+    <SplashScreen />
   );
 };
 
--- src/pages/SplashScreen/index.js
+++ src/pages/SplashScreen/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Logo } from '../../assets';
 
 const SplashScreen = () => {
     return (
-        <View>
-            <Text>Splash Page</Text>
+        <View style={{
+            backgroundColor: '#FFC700',
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <Logo />
+            <View style={{ height: 38 }} />
+            <Text style={{
+                fontSize: 32,
+                color: '#020202'
+            }}>Food Market</Text>
         </View>
     )
 }
