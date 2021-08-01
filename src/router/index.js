acf829e381c7971cd351ce727f3e0b2ac1e46a1d
bintang4974 <bintangramadhan4974@gmail.com>
2021-08-01 12:58:05 +0700

    adding library bottom tab navigator

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,62 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import { SignIn, SignUp, SignUpAddress, SplashScreen, SuccessSignUp } from '../pages';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+    Home,
+    Order,
+    Profile,
+    SignIn,
+    SignUp,
+    SignUpAddress,
+    SplashScreen,
+    SuccessSignUp
+} from '../pages';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="Order" component={Order} />
+            <Tab.Screen name="Profile" component={Profile} />
+        </Tab.Navigator>
+    )
+}
 
 const Router = () => {
     return (
         <Stack.Navigator>
             <Stack.Screen
-                name="SplashScreen" 
+                name="SplashScreen"
                 component={SplashScreen}
-                options={{headerShown: false}}
+                options={{ headerShown: false }}
             />
             <Stack.Screen
                 name="SignIn"
                 component={SignIn}
-                options={{headerShown: false}}
+                options={{ headerShown: false }}
             />
             <Stack.Screen
                 name="SignUp"
                 component={SignUp}
-                options={{headerShown: false}}
+                options={{ headerShown: false }}
             />
             <Stack.Screen
                 name="SignUpAddress"
                 component={SignUpAddress}
-                options={{headerShown: false}}
+                options={{ headerShown: false }}
             />
             <Stack.Screen
                 name="SuccessSignUp"
                 component={SuccessSignUp}
-                options={{headerShown: false}}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="MainApp"
+                component={MainApp}
+                options={{ headerShown: false }}
             />
         </Stack.Navigator>
     )
