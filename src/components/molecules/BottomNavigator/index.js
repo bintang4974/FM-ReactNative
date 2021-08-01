a1aa0fafbd9923e287753b7b110b8380e895236a
bintang4974 <bintangramadhan4974@gmail.com>
2021-08-01 13:27:48 +0700

    custom bottom navigator

---
+++ src/components/molecules/BottomNavigator/index.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { IcHomeOn, IcOrderOn, IcProfileOn } from '../../../assets';
+
+const Icon = ({ label }) => {
+    switch(label){
+        case 'Home':
+            return <IcHomeOn />
+        case 'Order':
+            return <IcOrderOn />
+        case 'Profile':
+            return <IcProfileOn />
+        default:
+            return <IcOrderOn />
+    }
+}
+
+const BottomNavigator = ({ state, descriptors, navigation }) => {
+    const focusedOptions = descriptors[state.routes[state.index].key].options;
+
+    if (focusedOptions.tabBarVisible === false) {
+        return null;
+    }
+
+    return (
+        <View style={styles.container}>
+            {state.routes.map((route, index) => {
+                const { options } = descriptors[route.key];
+                const label =
+                    options.tabBarLabel !== undefined
+                        ? options.tabBarLabel
+                        : options.title !== undefined
+                            ? options.title
+                            : route.name;
+
+                const isFocused = state.index === index;
+
+                const onPress = () => {
+                    const event = navigation.emit({
+                        type: 'tabPress',
+                        target: route.key,
+                        canPreventDefault: true,
+                    });
+
+                    if (!isFocused && !event.defaultPrevented) {
+                        navigation.navigate(route.name);
+                    }
+                };
+
+                const onLongPress = () => {
+                    navigation.emit({
+                        type: 'tabLongPress',
+                        target: route.key,
+                    });
+                };
+
+                return (
+                    <TouchableOpacity
+                        key={index}
+                        accessibilityRole="button"
+                        accessibilityState={isFocused ? { selected: true } : {}}
+                        accessibilityLabel={options.tabBarAccessibilityLabel}
+                        testID={options.tabBarTestID}
+                        onPress={onPress}
+                        onLongPress={onLongPress}
+                    >
+                        <Icon label={label} />
+                    </TouchableOpacity>
+                );
+            })}
+        </View>
+    );
+}
+
+export default BottomNavigator
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        paddingTop: 15,
+        paddingBottom: 13,
+        paddingHorizontal: 50,
+        justifyContent: 'space-between'
+    }
+})
