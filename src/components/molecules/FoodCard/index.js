a8387252e273df8f99670c8a4b2036c285136eb5
bintang4974 <bintangramadhan4974@gmail.com>
2021-08-02 16:43:23 +0700

    slicing food list item

--- src/components/molecules/FoodCard/index.js
+++ src/components/molecules/FoodCard/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
-import { IcStarOff, IcStarOn } from '../../../assets';
+import Rating from '../Rating';
 
 const FoodCard = ({ image }) => {
     return (
@@ -8,16 +8,7 @@ const FoodCard = ({ image }) => {
             <Image source={image} style={styles.image} />
             <View style={styles.content}>
                 <Text style={styles.text}>Cherry Healty</Text>
-                <View style={styles.ratingContainer}>
-                    <View style={styles.starContainer}>
-                        <IcStarOn />
-                        <IcStarOn />
-                        <IcStarOn />
-                        <IcStarOn />
-                        <IcStarOff />
-                    </View>
-                    <Text>4.5</Text>
-                </View>
+                <Rating />
             </View>
         </View>
     )
@@ -51,10 +42,4 @@ const styles = StyleSheet.create({
         fontFamily: 'Poppins-Regular',
         color: '#020202'
     },
-    ratingContainer: {
-        flexDirection: 'row'
-    },
-    starContainer: {
-        flexDirection: 'row'
-    }
 })
