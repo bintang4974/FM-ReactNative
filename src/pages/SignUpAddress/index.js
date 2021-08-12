f1f1e62eed11f921907a334d8c7b87ccd2b5bf73
bintang4974 <bintangramadhan4974@gmail.com>
2021-08-12 15:33:10 +0700

    integration register with redux

--- src/pages/SignUpAddress/index.js
+++ src/pages/SignUpAddress/index.js
@@ -1,39 +1,73 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import { Button, Gap, Header, Select, TextInput } from '../../components';
+import { useForm } from '../../utils';
+import { useDispatch, useSelector } from 'react-redux';
 
 const SignUpAddress = ({ navigation }) => {
+    const [form, setForm] = useForm({
+        phoneNumber: '',
+        address: '',
+        houseNumber: '',
+        city: 'Bandung',
+    });
+
+    const dispatch = useDispatch();
+    const registerReducer = useSelector(state => state.registerReducer);
+
+    const onSubmit = () => {
+        console.log('form: ', form);
+        const data = {
+            ...form,
+            ...registerReducer
+        }
+        console.log('data register: ', data);
+        // navigation.replace('SuccessSignUp');
+    }
+
     return (
-        <View style={styles.page}>
-            <Header
-                title="Address"
-                subTitle="Make sure it's valid"
-                onBack={() => { }}
-            />
-            <View style={styles.container}>
-                <TextInput
-                    label="Phone No."
-                    placeholder="Type your phone number"
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+            <View style={styles.page}>
+                <Header
+                    title="Address"
+                    subTitle="Make sure it's valid"
+                    onBack={() => { }}
                 />
-                <Gap height={16} />
-                <TextInput
-                    label="Address"
-                    placeholder="Type your address"
-                />
-                <Gap height={16} />
-                <TextInput
-                    label="House No."
-                    placeholder="Type your house number"
-                />
-                <Gap height={16} />
-                <Select label="City" />
-                <Gap height={24} />
-                <Button
-                    text="Sign Up Now"
-                    onPress={() => navigation.replace('SuccessSignUp')}
-                />
+                <View style={styles.container}>
+                    <TextInput
+                        label="Phone No."
+                        placeholder="Type your phone number"
+                        value={form.phoneNumber}
+                        onChangeText={(value) => setForm('phoneNumber', value)}
+                    />
+                    <Gap height={16} />
+                    <TextInput
+                        label="Address"
+                        placeholder="Type your address"
+                        value={form.address}
+                        onChangeText={(value) => setForm('address', value)}
+                    />
+                    <Gap height={16} />
+                    <TextInput
+                        label="House No."
+                        placeholder="Type your house number"
+                        value={form.houseNumber}
+                        onChangeText={(value) => setForm('houseNumber', value)}
+                    />
+                    <Gap height={16} />
+                    <Select
+                        label="City"
+                        value={form.city}
+                        onSelectChange={(value) => setForm('city', value)}
+                    />
+                    <Gap height={24} />
+                    <Button
+                        text="Sign Up Now"
+                        onPress={onSubmit}
+                    />
+                </View>
             </View>
-        </View>
+        </ScrollView>
     )
 }
 
